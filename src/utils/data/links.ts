3a2f48803d119917323830190304917ef64993c3
Yull Timoteo <yull.timoteo.z@uni.pe>
2024-05-24 14:37:34 -0500

    Style Completion: Header, NavBar

---
+++ src/utils/data/links.ts
@@ -0,0 +1,19 @@
+const linkList = [
+  {
+    href: "./",
+    content: "All Launches",
+    linkSvg: "src/svg/rockets.svg",
+  },
+  {
+    href: "/successful",
+    content: "Successful",
+    linkSvg: "src/svg/rocket.svg",
+  },
+  {
+    href: "./failures",
+    content: "Failures",
+    linkSvg: "src/svg/rocket-off.svg",
+  },
+];
+
+export { linkList };

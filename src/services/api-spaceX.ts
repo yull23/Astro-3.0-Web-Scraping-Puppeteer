9e9e3e9d1c84674b8cd95a003f7fb83b95f9839d
Yull Timoteo <yull.timoteo.z@uni.pe>
2024-05-24 17:09:54 -0500

    Finishing style on dynamic page

--- src/services/api-spaceX.ts
+++ src/services/api-spaceX.ts
@@ -1,6 +1,6 @@
 import type { Launch } from "../utils/types/spaceX.types";
 
-export async function getLaunchesSpaceX() {
+export async function getLaunchesSpaceX(id?: number) {
   const response = await fetch(import.meta.env.PUBLIC_API_URL_SPACEX);
   const launches = (await response.json()) as Launch[];
   return launches;
